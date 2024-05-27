baa3ba46ccc08e3f073966e3e4ee3c9771bc2aef
Malvern Gondo <malvernbright.95@gmail.com>
2024-05-27 12:59:40 +0200

    Corrected file handling for property creation form

--- src/pages/admin/CreateProperty.jsx
+++ src/pages/admin/CreateProperty.jsx
@@ -1,19 +1,57 @@
 import { Button, Col, Container, Form, Row } from "react-bootstrap"
 import { SideBar } from "./components/SideBar"
 import { useState } from "react"
-import { propertyFormDataPost } from "../schemas/propertyFormData"
+// import { propertyFormDataPost } from "../schemas/propertyFormData"
+import api from "../../config/api"
+import LoadingIndicator from "../components/LoadingIndicator"
 // import {  propertyFormDataPost } from "../schemas/propertyFormData";
 
 export const CreateProperty = () => {
+  const [isLoading, setIsLoading] = useState(false)
   const [title, setTitle] = useState("")
   const [description, setDescription] = useState("")
   const [size, setSize] = useState("")
   const [price, setPrice] = useState(0)
   const [tenure, setTenure] = useState("")
   const [propertyImage, setPropertyImage] = useState(null)
-  const { handleSubmit } = propertyFormDataPost({ title, description, size, price, tenure, propertyImage });
-  // const [frequency, setFrequency] = useState('Monthly');
-  // const { postData, handleSubmit, errors, onSubmit: createProperty } = PropertyCreationSchema();
+  // const { handleSubmit } = propertyFormDataPost({ title, description, size, price, tenure, propertyImage });
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true)
+    // Create a FormData object
+    const formData = new FormData();
+
+    // Append file to the formData object here
+    formData.append('title', title);
+    formData.append('description', description);
+    formData.append('size', size);
+    formData.append('price', price);
+    formData.append('tenure', tenure);
+    formData.append('property_image', propertyImage);
+
+    try {
+      // We will send formData object as a data to the API URL here.
+      api.post("/api/properties", formData, {
+        headers: { "Content-Type": "multipart/form-data" }
+      }).then((res) => {
+        alert("Property Posted Successfully");
+        setIsLoading(false)
+        console.log(res.data)
+      }).catch((error) => {
+        alert("Error")
+        setIsLoading(false)
+        console.error(error)
+      });
+    } catch (error) {
+      console.log(error)
+      setIsLoading(false)
+    }
+  }
+
+  const handleFileSelect = (event) => {
+    // we only get the selected file from input element's event
+    setPropertyImage(event.target.files[0])
+  }
   return (
     <Container fluid style={{ marginTop: "60px" }} >
       <Row>
@@ -59,11 +97,12 @@ export const CreateProperty = () => {
               </Form.Group>
               <Form.Group>
                 <Form.Label>Property Image:</Form.Label>
-                <input required={true} type="file" className="form-file form-control" onChange={(e) => setPropertyImage(e.target.files[0])} />
+                <input required={true} type="file" className="form-file form-control" onChange={handleFileSelect} />
                 {/* <p className="error">{errors.property_image?.message}</p> */}
               </Form.Group>
               <Form.Group>
-                <Button variant="outline-primary primary my-2" style={{ width: "inherit" }} type="submit">Create Property</Button>                  <p className="error"></p>
+                {isLoading ? <LoadingIndicator /> : <Button variant="outline-primary primary my-2" style={{ width: "inherit" }} type="submit">Create Property</Button>}
+                <p className="error"></p>
               </Form.Group>
             </Form>
 
