eba3ac7669ea3903ea26f5a55fed258ce6affb7e
Malvern Gondo <malvernbright.95@gmail.com>
2024-05-28 16:23:19 +0200

    Added docker

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
+  // base: "/",
   plugins: [react()],
-})
+  server:{
+    watch:{
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  }
+});
